3436a5518d8cf0bb5698f2666a05ae16518da15b
Julian Baltuttis <julianbaltuttis@gmail.com>
2020-05-12 09:18:13 +0200

    Init gitignore and lab doc.

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -1,6 +1,13 @@
 package org;
 
 import lombok.extern.log4j.Log4j;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
 
 @Log4j
 public class Main {
@@ -8,7 +15,10 @@ public class Main {
     public static void main(String[] args) {
         log.info("--> Main().");
         System.out.println("Hello World!");
+
         log.info("<-- Main().");
     }
 
 }
+
+
