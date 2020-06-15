8f863df1d242938d1fa461dd1d291ac8242aca68
Tim Stelzner <tim1409@gmx.net>
2020-06-15 13:15:16 +0200

    Refs #0003: Added class InMemorySearchEngine.java

---
+++ src/main/java/org/InMemorySearchEngine.java
@@ -0,0 +1,14 @@
+package org;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.MMapDirectory;
+import org.apache.lucene.store.RAMDirectory;
+
+
+public class InMemorySearchEngine {
+    StandardAnalyzer analyzer = new StandardAnalyzer();
+
+
+
+}
