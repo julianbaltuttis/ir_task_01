2086f06fa8d5c94524c27bda64dd5562d89372f0
Tim Stelzner <tim1409@gmx.net>
2020-06-24 18:17:00 +0200

    Refs #0005: Updated class InMemorySearchEngine.java

--- src/main/java/org/InMemorySearchEngine.java
+++ src/main/java/org/InMemorySearchEngine.java
@@ -1,14 +1,40 @@
 package org;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.MMapDirectory;
-import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONValue;
+
+import java.io.*;
 
 
 public class InMemorySearchEngine {
-    StandardAnalyzer analyzer = new StandardAnalyzer();
 
+    public JSONArray parseJSONFile() {
 
+        InputStream jsonFile = getClass().getResourceAsStream("filepath");
+        Reader readerJson = new InputStreamReader(jsonFile);
+
+        Object fileObjects = JSONValue.parse(readerJson);
+        JSONArray arrayObjects = (JSONArray)fileObjects;
+
+        return arrayObjects;
+    }
+    String indexPath;
+    String jsonFilePath;
+
+    IndexWriter indexWriter = null;
+
+    public InMemorySearchEngine(String indexPath, String jsonFilePath) {
+        this.indexPath = indexPath;
+        this.jsonFilePath =jsonFilePath;
+    }
+
+    public void createIndex(){
+        JSONArray jsonObjects = parseJSONFile();
+    }
 
 }
+
