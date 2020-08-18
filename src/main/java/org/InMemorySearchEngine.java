6f3b1da1b8d66cb2bf52f149d427aa9f2119af54
Tim Stelzner <tim1409@gmx.net>
2020-08-18 16:57:10 +0200

    Refs #0007: Fixed addDocuments method.

--- src/main/java/org/InMemorySearchEngine.java
+++ src/main/java/org/InMemorySearchEngine.java
@@ -5,6 +5,7 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.*;
@@ -47,9 +48,10 @@ public class InMemorySearchEngine {
         Reader readerJson = new InputStreamReader(jsonFile);
 
         Object fileObjects = JSONValue.parse(readerJson);
-        System.out.println(fileObjects);
+        JSONObject object = (JSONObject) fileObjects;
+        Object fileArray = object.get("arguments");
 
-        JSONArray arrayObjects = (JSONArray) fileObjects;
+        JSONArray arrayObjects = (JSONArray) fileArray;
         return arrayObjects;
     }catch(NullPointerException e){
         e.printStackTrace();
@@ -72,25 +74,25 @@ public class InMemorySearchEngine {
         return false;
     }
     public void addDocuments(JSONArray jsonObjects) {
-        for(JSONObject object :(List<JSONObject>) jsonObjects) {
-            System.out.println(object);
-        }
-        /*
+
         for(JSONObject object : (List<JSONObject>) jsonObjects){
             Document doc = new Document();
-            for(String field : (Set<String>) object.keySet()){
-                Class type = object.get(field).getClass();
-                if(type.equals(String.class)) {
-                    doc.add(new StringField(field, (String)object.get(field), Field.Store.NO));
-                }
-                else if(type.equals(List.class)){
-                    for(premise : (List)field)
-                    doc.add(new Field(field, (long)object.get(field), Field.Store.YES));
-                }
+            doc.add(new StringField("id", (String)object.get("id"), Field.Store.NO));
+            doc.add(new StringField("conclusion", (String)object.get("conclusion"), Field.Store.YES));
+
+            JSONArray premisesArray = (JSONArray)object.get("premises");
+            for(JSONObject premisObject : (List<JSONObject>) premisesArray) {
+                doc.add(new StringField("text",(String)premisObject.get("text"), Field.Store.NO));
+                doc.add(new StringField("stance",(String)premisObject.get("stance"), Field.Store.YES));
+            }
+
+            doc.add(new StringField("sourceID", (String)((JSONObject)object.get("context")).get("sourceId"), Field.Store.NO));
+
 
             }
-        }*/
+        }
     }
 
-}
+
+
 
