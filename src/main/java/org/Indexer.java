94bf7d535538b3fc7b8c81c0c0a835cb2ee9b4a0
Tim Stelzner <tim1409@gmx.net>
2020-08-23 17:27:07 +0200

    Refs #0012: Added jackson-core. Added experimental addDocuments method.

--- src/main/java/org/Indexer.java
+++ src/main/java/org/Indexer.java
@@ -1,7 +1,11 @@
 package org;
 
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import lombok.Setter;
+import lombok.extern.log4j.Log4j;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
@@ -16,10 +20,20 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
-
+@Log4j
 public class Indexer {
+
+    public static final String DOC_ARGUMENTS ="arguments";
+    public static final String DOC_ID ="id";
+    public static final String DOC_CONCLUSION="conclusion";
+    public static final String DOC_PREMISES="premises";
+    public static final String DOC_TEXT="text";
+    public static final String DOC_STANCE="stance";
+    public static final String DOC_CONTEXT="context";
+    public static final String DOC_SOURCE_ID="sourceId";
 
     private String indexPath = "";
 
@@ -36,26 +50,23 @@ public class Indexer {
     }
 
     public void createIndex(){
-        JSONArray jsonObjects = parseJSONFile();
+        //JSONArray jsonObjects = parseJSONFile();
         openIndex();
-        addDocuments(jsonObjects);
+        //addDocuments(jsonObjects);
+        addDocuments();
         finish();
     }
     public JSONArray parseJSONFile() {
     try {
-        InputStream jsonFile = getClass().getResourceAsStream(jsonFilePath);
+        BufferedInputStream jsonFile = (BufferedInputStream) getClass().getResourceAsStream(jsonFilePath);
         Reader readerJson = new InputStreamReader(jsonFile);
 
-        Object fileArray = ((JSONObject) JSONValue.parse(readerJson)).get("arguments");
-        readerJson.close();
-        jsonFile.close();
+        //Object fileArray = ((JSONObject) JSONValue.parse(readerJson)).get("arguments");
+        //readerJson.close();
+        //jsonFile.close();
 
-        return (JSONArray) fileArray;
-
+        return (JSONArray) ((JSONObject) JSONValue.parse(readerJson)).get("arguments");
     }catch(NullPointerException e){
-        e.printStackTrace();
-        return null;
-    } catch (IOException e) {
         e.printStackTrace();
         return null;
     }
@@ -79,9 +90,87 @@ public class Indexer {
 
             return true;
         } catch (Exception e) {
-            System.err.println("Error openin the index." + e.getMessage());
+            log.error("Error openin the index." + e.getMessage());
         }
         return false;
+    }
+    public void addDocuments() {
+        File jsonFile = new File(jsonFilePath);
+        JsonFactory jsonfactory = new JsonFactory();
+        int numberOfRecords = 0;
+        try {
+
+            JsonParser jsonParser = jsonfactory.createParser(jsonFile);
+            Document doc = new Document();
+
+            JsonToken jsonToken = jsonParser.nextToken();
+            while (jsonToken != JsonToken.END_OBJECT) {
+                String fieldName = jsonParser.getCurrentName();
+
+                if(DOC_ARGUMENTS.equals(fieldName)) {
+                    jsonToken = jsonParser.nextToken();
+
+                    while (jsonParser.nextToken() != JsonToken.END_ARRAY) {
+                        String argumentFieldName =jsonParser.getCurrentName();
+
+                        if(DOC_ID.equals(argumentFieldName)){
+                            jsonToken = jsonParser.nextToken();
+                            doc.add(new StringField(argumentFieldName, jsonParser.getText(), Field.Store.NO));
+                        }
+
+                        if(DOC_CONCLUSION.equals(argumentFieldName)){
+                            jsonToken =jsonParser.nextToken();
+                            doc.add(new StringField(argumentFieldName, jsonParser.getText(),Field.Store.YES));
+                        }
+
+                        if(DOC_PREMISES.equals(argumentFieldName)){
+                            jsonToken = jsonParser.nextToken();
+                            while (jsonParser.nextToken() != JsonToken.END_ARRAY) {
+                                String premiseFieldName = jsonParser.getCurrentName();
+
+                                if(DOC_TEXT.equals(premiseFieldName)) {
+                                    jsonToken = jsonParser.nextToken();
+                                    doc.add(new StringField(premiseFieldName, jsonParser.getText(), Field.Store.NO));
+                                }
+
+                                if(DOC_STANCE.equals(premiseFieldName)) {
+                                    jsonToken = jsonParser.nextToken();
+                                    doc.add(new StringField(premiseFieldName, jsonParser.getText(), Field.Store.YES));
+                                }
+                                else {
+                                    jsonToken = jsonParser.nextToken();
+                                }
+
+                            }
+                        }
+
+                        if(DOC_CONTEXT.equals(argumentFieldName)) {
+                            jsonToken = jsonParser.nextToken();
+                            while (jsonParser.nextToken() != JsonToken.END_OBJECT) {
+                                String contextFieldName = jsonParser.getCurrentName();
+
+                                if(DOC_SOURCE_ID.equals(contextFieldName)) {
+                                    jsonToken = jsonParser.nextToken();
+                                    doc.add(new StringField(contextFieldName, jsonParser.getText(), Field.Store.NO));
+                                }
+                                else {
+                                    jsonToken = jsonParser.nextToken();
+                                }
+                            }
+                        }
+                        if(jsonToken == JsonToken.END_OBJECT) {
+                            indexWriter.addDocument(doc);
+                            doc = new Document();
+                            numberOfRecords++;
+                        }
+                        jsonToken = jsonParser.nextToken();
+                    }
+                }
+                jsonToken = jsonParser.nextToken();
+            }
+        } catch(IOException e) {
+            log.error("Failed parsing JSON file"+ e.getMessage());
+        }
     }
     public void addDocuments(JSONArray jsonObjects) {
 
